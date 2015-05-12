3fe9088addb638eb11a3424a91b41b06036a4ef8
andrew <wertuk2@gmail.com>
2015-05-12 20:09:11 +0300

    t1: fixed 2

--- core/views.py
+++ core/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     context = {}
-    template = 'index.html'
+    template = 'core/index.html'
     return render(request, template, context)
 # Create your views here.
--- movies_tf/settings.py
+++ movies_tf/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'movies_tf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'apps/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'core/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
