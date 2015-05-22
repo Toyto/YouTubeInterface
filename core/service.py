e791fb6c483e6721e448799071f2ed4ed2421598
Ferens Dmitriy <ferensdima@gmail.com>
2015-05-22 23:15:26 +0300

    t3: Fixed indents

--- core/service.py
+++ core/service.py
@@ -6,6 +6,9 @@ from django.conf import settings
 from social.apps.django_app.default.models import UserSocialAuth
 
 
+_API_URL = 'https://www.googleapis.com/youtube/v3'
+
+
 class ChannelInfo:
 
     """
@@ -62,16 +65,18 @@ class VideoInfo:
 
 
 def get_channel_info(channel_id):
-    url = 'https://www.googleapis.com/youtube/v3/channels?\
-part=contentDetails%2C+snippet&id=\
-{channel_id}&key={GOOGLE_API_KEY}'
+    url = (
+        _API_URL
+        '/channels?part=contentDetails%2C+snippet'
+        '&id={channel_id}'
+        '&key={GOOGLE_API_KEY}'
+    )
     json_url = url.format(
         channel_id=str(channel_id),
         GOOGLE_API_KEY=str(settings.GOOGLE_API_KEY)
     )
-    r = requests.get(json_url)
-    # Convert it to a Python dictionary
-    data = json.loads(r.text)
+
+    data = json.loads(requests.get(json_url).text)
     into_items = data['items'][0]
     into_snippet_thumbnail = into_items['snippet']['thumbnails']
     into_items_snippet = into_items['snippet']
@@ -99,8 +104,7 @@ def get_author_of_video(channel_id):
     except Author.DoesNotExist:
         return Author.objects.create(
             google_uid=channel_info.google_plus_id,
-            user=get_user_for_video_author(
-                channel_info.google_plus_id),
+            user=get_user_for_video_author(channel_info.google_plus_id),
             name=channel_info.title,
             avatar_url=channel_info.medium_thumbnail
         )
@@ -116,14 +120,17 @@ def get_video_id(url):
 
 
 def get_video_json(video_id):
-    video_url = 'https://www.googleapis.com/youtube/v3/videos?\
-part=snippet&id={video_id}&key={GOOGLE_API_KEY}'
+    url = (
+        _API_URL
+        '/videos?part=snippet'
+        '&id={video_id}'
+        '&key={GOOGLE_API_KEY}'
+    )
     json_url = video_url.format(
         video_id=str(video_id),
         GOOGLE_API_KEY=str(settings.GOOGLE_API_KEY)
     )
-    r = requests.get(json_url)
-    data = json.loads(r.text)
+    data = json.loads(requests.get(json_url).text)
     return data
 
 
@@ -134,7 +141,6 @@ def get_video_info(url):
     channel_id = into_items['snippet']['channelId']
     into_snippet_thumbnail = into_items['snippet']['thumbnails']
     into_items_snippet = into_items['snippet']
-
     return VideoInfo(
         title=into_items_snippet['title'],
         video_id=into_items['id'],
