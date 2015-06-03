69c713ad37299da3ee815a85c2f2afe3515540b1
andrew <wertuk2@gmail.com>
2015-06-03 16:41:27 +0300

    t4: add core/static

---
+++ core/static/core/publish_query.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+            $('#results').hide();
+            $('#player').hide();
+            $('#publish').hide();
+            $('#checkboxes').hide();
+            $('#btn_go').click(function(){
+                $.ajax({
+                  type: "POST",
+                  url: "http://127.0.0.1:8000/ajax_publish/",
+                  data: $('#url_form').serialize(),
+                  dataType: 'json',
+                  success: function(json){
+                    console.log(json['video_info']);
+                    var author = json['video_info']['author'];
+                    var title = json['video_info']['title'];
+                    var description = json['video_info']['description'];
+                    var video_id = json['video_info']['video_id'];
+                    $('#title').html(title);
+                    $('#description').html(description);
+                    $('#author').html(author);
+                    $('#player').html(
+                        "<iframe id='ytplayer' type='text/html' width='640' height='390' src='http://www.youtube.com/embed/"+ video_id + "' frameborder='0'></iframe>"
+                    );
+                    $('#player').show();
+                    $('#results').show('slow');
+                    $('#publish').show('slow');
+                    $('#checkboxes').show('slow');
+                    $('.checkbox').checkbox('attach events', '.check.button', 'check');
+                    $('.checkbox').checkbox('attach events', '.uncheck.button', 'uncheck');
+                  }
+                });
+            });
+      });
