47db57b748eb888b9a8606eef43372c1b4a411d4
andrew <wertuk2@gmail.com>
2015-06-23 21:03:33 +0300

    t6: added yt-player in index

--- core/static/core/show_videos.js
+++ core/static/core/show_videos.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
+  $('.special.cards .image').dimmer({
+    on: 'hover'
+    });
   $('.rating').rating('disable');
-  $(".category-block").on("click", ".show-more-btn", function(event){
+  $("body").on("click", ".show-more-btn", function(event){
       var button = $(event.target);
       var category_id = button.attr("data-category-id");
       var video_count = parseInt(button.attr("data-video-count"));
@@ -14,7 +17,21 @@ $(document).ready(function(){
           var parent = container.parent();
           container.replaceWith(data);
           $(parent).find('.rating').rating('disable');
+          $(parent).find('.special.cards .image').dimmer({
+            on: 'hover'
+            });
         }
       });
+  });
+  $("body").on("click", ".card .dimmable.image", function(event){
+    var video_id = $(this).attr("id");
+    $('#ytplayer_for'+video_id).replaceWith(
+              "<iframe id='ytplayer' type='text/html' width='100%' height='500px' src='http://www.youtube.com/embed/"+ video_id + "?autoplay=1&enablejsapi=1&vq=hd720' frameborder='0'></iframe>"
+          );
+    $("#modal"+video_id).modal("setting", {
+        onHidden: function () {
+          $('.modal.transition').remove();
+        }
+    }).modal("show");
   });
 });
